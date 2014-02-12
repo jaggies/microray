b6af531f918da5dd0e82c6cad726b0a7d84ba9e9
jaggies <jaggiesjim@gmail.com>
2014-02-12 02:23:09 -0800

    Add perspective camera and triangle test case to main.

--- src/main.c
+++ src/main.c
@@ -8,29 +8,37 @@
 #include <stdio.h>
 #include <assert.h>
 #include "sphere.h"
+#include "triangle.h"
 #include "pointlight.h"
 #include "hit.h"
 #include "phongshader.h"
+#include "perspectivecamera.h"
 
 #define XRES 1024
 #define YRES XRES
 #define MAXSHAPES 10
 #define MAXLIGHTS 8
-#define MAXDEPTH 1 // max number of reflected rays
+#define MAXDEPTH 2 // max number of reflected rays
 #define RAY_EPSILON 1.0e-5f
 
 typedef struct World {
     Shape* shapes[MAXSHAPES];
     Light* lights[MAXLIGHTS];
+    Camera* camera;
     int nShapes;
     int nLights;
     Vec3 background; // background colors
     float epsilon; // intersection slop
 } World;
 
+extern Camera* makeCamera();
+
 World* createWorld() {
     World* world = (World*) malloc(sizeof(World));
     world->epsilon = RAY_EPSILON;
+    world->nShapes = 0;
+    world->nLights = 0;
+    world->camera = makeCamera();
     Vec3 diffuse, specular, ambient;
     makeVec3(0.5,0.5,0.5,&specular);
     makeVec3(0.1,0.1,0.1,&ambient);
@@ -38,16 +46,28 @@ World* createWorld() {
     Shader* red = createPhongShader(&diffuse,  &specular,  &ambient, 20.0f, 1.4f, 1.0f, 0.5f);
     makeVec3(0.0,0.0,0.5,&diffuse);
     Shader* blu = createPhongShader(&diffuse,  &specular,  &ambient, 20.0f, 1.4f, 1.0f, 0.5f);
-    world->nShapes = 0;
-    world->nLights = 0;
+    Shader* mirror = createPhongShader(&diffuse, &specular, &ambient, 20.0f, 1.4, 1.0f, 0.0f);
     world->shapes[world->nShapes++] = createSphere(0.25, 0, 0, 0.25, red);
     world->shapes[world->nShapes++] = createSphere(-0.25, 0, 0, 0.25, blu);
+    float plane[][3] = { {-1, -0.25, -1}, {1, -0.25, -1}, {1, -0.25, 1}, {-1, -0.25, 1} };
+    world->shapes[world->nShapes++] = createTriangle((Vec3*) plane[0], (Vec3*) plane[1],
+            (Vec3*) plane[2], mirror);
+    world->shapes[world->nShapes++] = createTriangle((Vec3*) plane[2], (Vec3*) plane[3],
+            (Vec3*) plane[0], mirror);
     Vec3 point, color;
-    makeVec3(5,5,5,&point);
+    makeVec3(5,5,5, &point);
     makeVec3(1,1,1, &color);
     world->lights[world->nLights++] = createPointLight(&point, &color);
     makeVec3(0.2, 0.3, 0.7, &world->background);
     return world;
+}
+
+Camera* makeCamera() {
+    Vec3 from; makeVec3(1, 1, 1, &from);
+    Vec3 at; makeVec3(0, 0, 0, &at);
+    Vec3 up; makeVec3(0, 1, 0, &up);
+    float aspect =  (float) XRES / YRES;
+    return (Camera*) createPerspectiveCamera(&from, &at, &up, 45.0f, aspect);
 }
 
 void createRay(float u, float v, Ray* ray) {
@@ -129,16 +149,15 @@ void putPixel(int x, int y, Vec3* color) {
 
 int main(int argc, char **argv)
 {
-    int n = 0;
     World* world = createWorld();
     float du = 1.0f / XRES, dv = 1.0f / YRES;
-    float v = -0.5f + dv * 0.5f; // emit ray from pixel centers
-    Ray ray;
+    float v = 0.0f + dv * 0.5f; // emit ray from pixel centers
     for (int h = 0; h < YRES; h++, v += dv) {
-        float u = -0.5f + du * 0.5f;
+        float u = 0.0f + du * 0.5f;
         for (int w = 0; w < XRES; w++, u += du) {
-            createRay(u, v, &ray);
+            Ray ray;
             Vec3 color;
+            world->camera->op.makeRay(world->camera, u, 1.0f-v, &ray);
             trace(&ray, world, &color, MAXDEPTH);
             putPixel(w, h, &color);
         }
