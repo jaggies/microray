766ae7a0bf92bb4753d4b54bc12e34a3d0903ef8
jaggies <jaggiesjim@gmail.com>
2018-03-24 01:22:11 -0700

    Fix crash on Amiga due to memset bug.

--- src/world.c
+++ src/world.c
@@ -7,7 +7,7 @@
 
 World* createWorld() {
     World* world = (World*) malloc(sizeof(World));
-    memset(world, sizeof(World), 0);
+    memset(world, 0, sizeof(World));
     printf("Created world, size = %u, ptr=%x, nLights=%d\n",
              sizeof(World), (size_t) world, world->nLights);
     world->epsilon = RAY_EPSILON;
