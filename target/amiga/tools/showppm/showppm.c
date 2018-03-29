e08ef703ad0abb843db06f635f8f1cc406b9fdc7
jaggies <jaggiesjim@gmail.com>
2018-03-29 01:50:15 -0700

    Get showppm working with HAM mode

--- target/amiga/tools/showppm/showppm.c
+++ target/amiga/tools/showppm/showppm.c
@@ -9,27 +9,31 @@ struct IntuitionBase *IntuitionBase = 0;
 
 int main() {
     struct NewScreen Screen1 = {
-        0,0,640,200,4,             /* Screen of 640 x 480 of depth 8 (2^8 = 256 colours)    */
+        0,0,320,200,6,             /* Screen of 640 x 480 of depth 8 (2^8 = 256 colours)    */
         DETAILPEN, BLOCKPEN,
-        HIRES,                     /* see graphics/view.h for view modes */
-        PUBLICSCREEN,              /* Screen types */
+        HAM,                     /* see graphics/view.h for view modes */
+        CUSTOMSCREEN,              /* Screen types */
         NULL,                      /* Text attributes (use defaults) */
         "Pixel Screen", 
         NULL,
         NULL
     };
 
-	IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 39);
+    IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 39);
 
-	if (!IntuitionBase) {
-		printf("Can't open Intuition\n");
-	}
+    ShowTitle((struct Screen*) &Screen1, FALSE);
 
-	printf("Opening screen...\n");
+    if (!IntuitionBase) {
+        printf("Can't open Intuition\n");
+    }
+
     struct Screen *myScreen = OpenScreen(&Screen1); 
     Delay(500);
     if (myScreen) {
-		printf("Success!!!  Closing...\n");
-		CloseScreen((struct Screen*)&Screen1);
-	}
+        CloseScreen((struct Screen*)&Screen1);
+    }
+
+    if (IntuitionBase) {
+        CloseLibrary((struct Library*) IntuitionBase);
+    }
 } 
