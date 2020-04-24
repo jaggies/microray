35106d632c12eaad6e2002b0413d56d26e3f0ec1
jaggies <jaggiesjim@gmail.com>
2020-04-23 23:47:59 -0700

    Add dithering code for ARM target (untested)

--- apps/arm/armsimple.c
+++ apps/arm/armsimple.c
@@ -20,19 +20,42 @@
 #include "loader.h"
 #include "netpbm.h"
 #include "testload.h"
+#include "dither.h"
+
+#define RBITS 3
+#define GBITS 3
+#define BBITS 2
 
 #ifdef PROFILE
 long intersections = 0;
 #endif /* PROFILE */
 
 __attribute__((weak))
+void dot(int x, int y, int index) {
+
+}
+
+__attribute__((weak))
 void pixel(uint16_t x, uint16_t y, uint8_t* rgb, void* userdata) {
 	static int line = -1;
+
 	// Default implementation does nothing
 	if (line != y) {
 	    printf("Line %d\n", y);
 		line = y;
 	}
+    #ifdef USE_ERROR_DIFFUSION
+	static int rerr, gerr, berr;
+    int rx = diffusion_dither(1<<8, 1<<RBITS, rgb[0], &rerr);
+    int gx = diffusion_dither(1<<8, 1<<GBITS, rgb[1], &gerr);
+    int bx = diffusion_dither(1<<8, 1<<BBITS, rgb[2], &berr);
+    #else
+    uint8_t rx = ordered_dither(1<<8, 1<<RBITS, x, y, rgb[0]);
+    uint8_t gx = ordered_dither(1<<8, 1<<GBITS, x, y, rgb[1]);
+    uint8_t bx = ordered_dither(1<<8, 1<<BBITS, x, y, rgb[2]);
+    #endif
+    int index = (rx << (GBITS + BBITS)) | (gx << BBITS) | bx;
+    dot(x, 1024-y, index);
 }
 
 int main(int argc, char **argv)
